5b437c52024c775f18a9c02ba036f3cb80d1d19b
Bernard Iorver <bernard.benion.iorver@hotmail.com>
2019-10-20 04:34:56 +0100

    Updates

---
+++ precache-manifest.04ff25c84cf53741e2fa8455ae2f5a8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0431da29a08705b7c0703006e4719f5",
+    "url": "/validation/index.html"
+  },
+  {
+    "revision": "70ebd0eb5c7399395b79",
+    "url": "/validation/static/css/main.b0b5cb38.chunk.css"
+  },
+  {
+    "revision": "ed18e9a76273bbe9bdf6",
+    "url": "/validation/static/js/2.addccc62.chunk.js"
+  },
+  {
+    "revision": "70ebd0eb5c7399395b79",
+    "url": "/validation/static/js/main.847f5142.chunk.js"
+  },
+  {
+    "revision": "734f9db654dc0e4baba9",
+    "url": "/validation/static/js/runtime~main.464b7188.js"
+  }
+]);
